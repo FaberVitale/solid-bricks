6736a75f9bf2a139ee200f7aeca1fa7a64e37a59
FaberVitale <faber.vitale@gmail.com>
2022-05-29 16:25:02 +0200

    fix: codesandbox not working

--- examples/barcode-generator/vite.config.ts
+++ examples/barcode-generator/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ['jsbarcode'],
+  },
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
